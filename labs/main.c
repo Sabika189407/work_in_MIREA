d3484c24348941f1555af2a6dec79a50f812d9f3
Sabika <pda3pda45@gmail.com>
2025-11-20 10:43:59 +0300

    started making lab with calculator

---
+++ labs/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int check_imput(int* size, char** ar)
+{
+	if (size == NULL || ar == NULL)
+	{
+		printf("pointers are NULL!\n");
+		return 1;
+	}
+	if (*size <= 2  || (*size-1) % 3 != 0)
+	{
+		printf("incorret imput\n");
+		for (int i = 1; i < size; i++)
+		{
+			if (isdigit(ar[i]))
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	printf("%d", argc);
+	check_imput(&argc, argv);
+	return 0;
+}
