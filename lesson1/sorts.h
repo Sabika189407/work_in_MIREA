38d146cde408349643eba9c384561df8c8c079ae
Sabika <pda3pda45@gmail.com>
2026-03-16 11:55:38 +0300

    9 variations of sorts for array with time testing -working-

---
+++ lesson1/sorts.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <chrono>
+
+#pragma once
+
+void bubbleSort_stupid(int*, int);
+void insertion_sort(int*, int);
+void selectionSort(int*, int);
+void merge_sort(int*, int, int);
+void quick_sort(int*, int);
+void radix_sort(int*, int);
+void digit_sort(int*, int, int);
+int Stalin_sort(int*, int);
+void cocktail_sort(int*, int);
+void heap_sort(int*, int);
